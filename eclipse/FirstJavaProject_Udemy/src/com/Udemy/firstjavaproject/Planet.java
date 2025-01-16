622c2433846acfb32594d9929d802fc25ba94229
MOON <experiment.wired@gmail.com>
2025-01-16 12:26:18 +0900

    test1

---
+++ eclipse/FirstJavaProject_Udemy/src/com/Udemy/firstjavaproject/Planet.java
@@ -0,0 +1,11 @@
+package com.Udemy.firstjavaproject;
+
+public class Planet {
+	void revolve() {
+        System.out.println("Revolve");
+    }
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.revolve();
+    }
+}
---
+++ eclipse/FirstJavaProject_Udemy/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module FirstJavaProject_Udemy {
+}

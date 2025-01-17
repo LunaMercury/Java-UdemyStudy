c32beb1d948066f38ff9ea8b5cdd60d3b4963f7f
MOON <experiment.wired@gmail.com>
2025-01-17 12:05:12 +0900

    changed folder name

--- eclipse/FirstJavaProject_Udemy/src/com/Udemy/firstjavaproject/test.java
+++
@@ -1,5 +0,0 @@
-package com.Udemy.firstjavaproject;
-
-public class test {
-
-}
--- udemyStudy-eclipse/FirstJavaProject_Udemy/src/com/Udemy/firstjavaproject/perfectNumberScanner.java
+++ udemyStudy-eclipse/FirstJavaProject_Udemy/src/com/Udemy/firstjavaproject/perfectNumberScanner.java
@@ -1,10 +1,7 @@
 package com.Udemy.firstjavaproject;
 
-public class testRunner {
-
+public class perfectNumberScanner {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
